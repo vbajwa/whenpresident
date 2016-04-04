6d0d3ac8a7955a229ed75cf4cab412fb7baef6e9
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-04 11:46:15 -0400

    Connected to Mongoose

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,15 @@
+var mongoose  = require("mongoose");
+
+var CandidateSchema = new mongoose.Schema(
+  {
+    name: String,
+    year: Number
+  }
+);
+
+mongoose.model("Candidate", CandidateSchema);
+mongoose.connect("mongodb://localhost/whenpresident");
+
 var seedData = require("./seeds.json");
 module.exports = {
   candidates: seedData
