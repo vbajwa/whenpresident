4ce67f711f0355ea229697b0abc419de98420fde
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-04 14:18:43 -0400

    Updates a candidate in the database

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ app.post("/candidates", function(req, res){
   });
 });
 
+app.post("/candidates/:name", function(req, res){
+  Candidate.findOneAndUpdate({name: req.params.name}, req.body.candidate, {new: true}).then(function(candidate){
+    res.redirect("/candidates/" + candidate.name);
+  });
+});
+
 app.listen(app.get("port"), function(){
   console.log("It's aliiive!");
 });
