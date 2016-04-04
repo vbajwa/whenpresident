6e8e5cda5b27c6c135bcfd70b36bfacc39d626c7
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-04 19:33:29 -0400

    Added root route

--- public/js/app.js
+++ public/js/app.js
@@ -8,11 +8,12 @@
   ])
   .config([
     "$stateProvider",
+    "$urlRouterProvider",
     "$locationProvider",
     Router
   ]);
 
-  function Router($stateProvider, $locationProvider){
+  function Router($stateProvider, $urlRouterProvider, $locationProvider){
     $locationProvider.html5Mode(true);
     $stateProvider
     .state("welcome", {
@@ -23,5 +24,6 @@
       url: "/candidates",
       template: "This is the candidates index route."
     });
+    $urlRouterProvider.otherwise("/");
   }
 })();
