688295f740bfb324ccdf131114715f3f8675131d
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-04 12:08:25 -0400

    Shows db candidate in candidates#show

--- index.js
+++ index.js
@@ -29,15 +29,10 @@ app.get("/candidates", function(req, res){
 });
 
 app.get("/candidates/:name", function(req, res){
-  var desiredName = req.params.name.toLowerCase();
-  var candidateOutput;
-  db.candidates.forEach(function(candidate){
-    if(desiredName === candidate.name.toLowerCase()){
-      candidateOutput = candidate;
-    }
-  });
-  res.render("candidates-show", {
-    candidate: candidateOutput
+  Candidate.findOne({name: req.params.name}).then(function(candidate){
+    res.render("candidates-show", {
+      candidate: candidate
+    });
   });
 });
 
