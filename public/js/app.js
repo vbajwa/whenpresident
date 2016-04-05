36981a42710fe077073491c9f748af8400909800
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-05 12:12:35 -0400

    Added show route

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,8 @@
     Router
   ])
   .factory("Candidate", Candidate)
-  .controller("indexCtrl", indexCtrl);
+  .controller("indexCtrl", indexCtrl)
+  .controller("showCtrl", showCtrl);
 
   function Router($stateProvider, $urlRouterProvider, $locationProvider){
     $locationProvider.html5Mode(true);
@@ -26,6 +27,12 @@
       url: "/candidates",
       templateUrl: "/public/html/candidates-index.html",
       controller: "indexCtrl",
+      controllerAs: "vm"
+    })
+    .state("show", {
+      url: "/candidates/:name",
+      templateUrl: "/public/html/candidates-show.html",
+      controller: "showCtrl",
       controllerAs: "vm"
     });
     $urlRouterProvider.otherwise("/");
@@ -42,6 +49,18 @@
   function indexCtrl(Candidate){
     var vm = this;
     vm.candidates = Candidate.all;
+  }
+
+  showCtrl.$inject = [ "$stateParams", "Candidate" ];
+  function showCtrl($stateParams, Candidate){
+    var vm = this;
+    Candidate.all.$promise.then(function(){
+      Candidate.all.forEach(function(candidate){
+        if(candidate.name === $stateParams.name){
+          vm.candidate = candidate;
+        }
+      });
+    });
   }
 
 })();
