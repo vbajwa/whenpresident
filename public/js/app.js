00dad0aa670a0c68195c82618cf2b40a97ac8c5c
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-05 11:48:56 -0400

    Added welcome route

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
-console.log("Working!");
+"use strict";
+
+(function(){
+  angular
+  .module("whenPresident", [
+    "ngResource",
+    "ui.router"
+  ])
+  .config([
+    "$stateProvider",
+    Router
+  ]);
+
+  function Router($stateProvider){
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/public/html/app-welcome.html"
+    });
+  }
+})();
