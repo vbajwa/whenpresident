974161355776a91875b2b352b0ca05ad8e5e5dfe
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-06 23:04:51 -0400

    Added Twitter environment variables

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ var app     = express();
 var Candidate = mongoose.model("Candidate");
 
 process.env.session_secret = env.session_secret;
+process.env.t_callback_url = env.t_callback_url;
+process.env.t_consumer_key = env.t_consumer_key;
+process.env.t_consumer_secret = env.t_consumer_secret;
 
 app.use(session({
   secret: process.env.session_secret,
