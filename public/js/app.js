f3f993e98d2c7693bad6a98180bcd538101b6c9c
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-06 22:58:36 -0400

    Added welcome page

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,21 @@
 "use strict";
 
 (function(){
-  
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    Router
   ]);
 
+  function Router($stateProvider){
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/html/candidates-welcome.html"
+    });
+  }
 })();
