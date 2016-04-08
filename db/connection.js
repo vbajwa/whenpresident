e23f50f9eb7a6f68fea8000828d8b3eadb5300d9
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2016-04-08 14:23:24 -0400

    connection js file updated

--- db/connection.js
+++ db/connection.js
@@ -8,5 +8,10 @@ var CandidateSchema = new mongoose.Schema(
 );
 
 mongoose.model("Candidate", CandidateSchema);
+if(process.env.NODE_ENV == "production"){
+  mongoose.connect(process.env.MONGOLAB_URI);
+}else{
+  mongoose.connect("mongodb://localhost/whenpresident");
+}
 
 module.exports = mongoose;
